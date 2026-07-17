5bf26b90fea9c30258abb93a5d01b67f557ade27
anniefebia9999 <anniefebia9999@gmail.com>
2026-07-17 12:31:55 +0530

    Update CORS to new Vercel URL

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
         	    "http://localhost:4200",
-        	    "https://stylence.vercel.app"
+        	    "https://stylence-shop.vercel.app"
         	));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
